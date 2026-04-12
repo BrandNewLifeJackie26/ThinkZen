b841100361a2fbdf76822296440b0e00d7b1c3d7
BrandNewLifeJackie26 <53093845+BrandNewLifeJackie26@users.noreply.github.com>
2026-04-11 23:25:09 -0400

    apply comments

--- db/index.ts
+++ db/index.ts
@@ -2,5 +2,17 @@ import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as schema from './schema'
 
-const sqlite = new Database('thinkzen.db')
-export const db = drizzle(sqlite, { schema })
+const dbPath = process.env.THINKZEN_DB_PATH ?? 'thinkzen.db'
+declare global {
+  // eslint-disable-next-line no-var
+  var __thinkzenSqlite__: Database.Database | undefined
+  // eslint-disable-next-line no-var
+  var __thinkzenDrizzle__:
+    | ReturnType<typeof drizzle<typeof schema>>
+    | undefined
+}
+const sqlite = globalThis.__thinkzenSqlite__ ?? new Database(dbPath)
+const db = globalThis.__thinkzenDrizzle__ ?? drizzle(sqlite, { schema })
+globalThis.__thinkzenSqlite__ = sqlite
+globalThis.__thinkzenDrizzle__ = db
+export { sqlite, db }
